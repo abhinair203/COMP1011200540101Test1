6aac5833742cbdb0522de651384e88947a324f16
abhinair203 <abhinair203@gmail.com>
2023-11-14 16:21:23 -0500

    Question 2a complete

--- COMP1011200540101Test1/src/main/java/com/example/comp1011200540101test1/HelloApplication.java
+++ COMP1011200540101Test1/src/main/java/com/example/comp1011200540101test1/HelloApplication.java
@@ -12,9 +12,9 @@ import java.util.Date;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Employee e=new Employee(2," ABC","ABC", new Date(),"HR","abc@gmail.com",50000);
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        Employee e=new Employee(2,"ABC","ABC", new Date(),"HR","abc@gmail.com",50000);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
